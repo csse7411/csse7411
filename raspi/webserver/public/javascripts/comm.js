a016a245ff9f7498031164d593776b34a87bf8e8
Romaan Ahmed <romaan@djenie.com>
2014-06-07 16:19:45 +1000

    Receive file name test

--- raspi/webserver/public/javascripts/comm.js
+++ raspi/webserver/public/javascripts/comm.js
@@ -7,7 +7,7 @@ $(document).ready(function(e) {
 		    data: "graph="+document.getElementById("graph").value+"&sensor="+document.getElementById("sensor").value+"&sensortype="+document.getElementById("sensortype").value+"&startime="+document.getElementById("starttime").value+"&endtime="+document.getElementById("endtime").value,
 		    type: 'GET',
 		    success: function (resp) {
-		        alert(resp);
+		        alert(resp);		        
 		    },
 		    error: function(e) {
 		        alert('Error: '+e);
