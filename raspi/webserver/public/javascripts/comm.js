44c862e38d4ad07886ece19e0098b23ee823ec1e
Romaan Ahmed <romaan@djenie.com>
2014-06-08 05:29:32 +1000

    Waiting added

--- raspi/webserver/public/javascripts/comm.js
+++ raspi/webserver/public/javascripts/comm.js
@@ -1,16 +1,28 @@
 var myURL="http://10.0.0.1:4000?";
 
+function ShowProgressAnimation() {
+    $("#loading-div-background").show();
+} 
+
+function HideProgressAnimation() {
+    $("#loading-div-background").hide();
+} 
+
 $(document).ready(function(e) {
+	 $("#loading-div-background").css({ opacity: 0.8 });
+
 	 $("#sendButton").click(function() {
+	 	ShowProgressAnimation();
         $.ajax({
 		    url: myURL+"callback=?",
 		    data: "graph="+document.getElementById("graph").value+"&zone="+document.getElementById("zone").value+"&sensor="+document.getElementById("sensor").value+"&sensortype="+document.getElementById("sensortype").value+"&startime="+document.getElementById("starttime").value+"&endtime="+document.getElementById("endtime").value,
 		    type: 'GET',
 		    success: function (resp) {
-		        alert(resp);
+		    	HideProgressAnimation();		        
 		        $("#graphimage").attr('src', 'images/'+resp);
 		    },
 		    error: function(e) {
+		    	HideProgressAnimation();
 		        alert('Error: '+e);
 		    }  
 		});            
